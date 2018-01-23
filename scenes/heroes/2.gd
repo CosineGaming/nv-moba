cf062334bab32454d200f4d7ea4109d0e94cb512
Luna <judahiii@gmail.com>
2018-01-23 00:40:10 -0500

    Add repelling (toggle with E) to Hero 2

--- scenes/heroes/2.gd
+++ scenes/heroes/2.gd
@@ -3,6 +3,16 @@ extends "res://scripts/player.gd"
 #func _ready():
 #	get_node("Area").set_gravity_distance_scale(10)
 
+var is_repelling = false
+
 func _process(delta):
-	print(get_translation())
+	if Input.is_action_just_pressed("hero_2_switch_gravity"):
+		# Press button twice to cancel
+		var area = get_node("Area")
+		area.set_gravity(-area.get_gravity())
+		is_repelling = !is_repelling
+		if is_repelling:
+			get_node("RepellingHUD").set_text("/\\")
+		else:
+			get_node("RepellingHUD").set_text("\\/")
 	#get_node("Area").set_gravity_vector(get_translation())
