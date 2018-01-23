1867d8afda58fb329a90b0b4a74ed0797e69f538
Luna <judahiii@gmail.com>
2018-01-23 01:33:02 -0500

    [Hero 2] Make gravity switch in multiplayer

--- scripts/heroes/2.gd
+++ scripts/heroes/2.gd
@@ -1,18 +1,18 @@
 extends "res://scripts/player.gd"
 
-#func _ready():
-#	get_node("Area").set_gravity_distance_scale(10)
-
 var is_repelling = false
 
 func _process(delta):
 	if is_network_master():
 		if Input.is_action_just_pressed("hero_2_switch_gravity"):
 			# Press button twice to cancel
-			var area = get_node("Area")
-			area.set_gravity(-area.get_gravity())
+			rpc("switch_gravity")
 			is_repelling = !is_repelling
 			if is_repelling:
 				get_node("RepellingHUD").set_text("/\\")
 			else:
 				get_node("RepellingHUD").set_text("\\/")
+
+sync func switch_gravity():
+	var area = get_node("Area")
+	area.set_gravity(-area.get_gravity())
