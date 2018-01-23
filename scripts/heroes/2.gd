45f3a199559840c9fbef894aed750ae427c33633
Luna <judahiii@gmail.com>
2018-01-23 00:51:22 -0500

    Fix Hero 1's E affecting Hero 2's E

--- scripts/heroes/2.gd
+++ scripts/heroes/2.gd
@@ -6,13 +6,13 @@ extends "res://scripts/player.gd"
 var is_repelling = false
 
 func _process(delta):
-	if Input.is_action_just_pressed("hero_2_switch_gravity"):
-		# Press button twice to cancel
-		var area = get_node("Area")
-		area.set_gravity(-area.get_gravity())
-		is_repelling = !is_repelling
-		if is_repelling:
-			get_node("RepellingHUD").set_text("/\\")
-		else:
-			get_node("RepellingHUD").set_text("\\/")
-	#get_node("Area").set_gravity_vector(get_translation())
+	if is_network_master():
+		if Input.is_action_just_pressed("hero_2_switch_gravity"):
+			# Press button twice to cancel
+			var area = get_node("Area")
+			area.set_gravity(-area.get_gravity())
+			is_repelling = !is_repelling
+			if is_repelling:
+				get_node("RepellingHUD").set_text("/\\")
+			else:
+				get_node("RepellingHUD").set_text("\\/")
