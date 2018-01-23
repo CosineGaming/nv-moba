b8521f2384893ba2e56b596e38bd69bb88eb51cd
Luna <judahiii@gmail.com>
2018-01-23 01:36:56 -0500

    Make Hero 2 interface display only on master

--- scripts/heroes/2.gd
+++ scripts/heroes/2.gd
@@ -4,15 +4,31 @@ extends "res://scripts/player.gd"
 #	get_node("Area").set_gravity_distance_scale(10)
 
 var is_repelling = false
+var repel_hud
+
+func _ready():
+	print("nutty")
+	if is_network_master():
+		print("netty")
+		repel_hud = Label.new()
+		repel_hud.set_name("RepellingHUD")
+		repel_hud.set_text("\\/")
+		repel_hud.set_position(Vector2(0, -20))
+		repel_hud.set_visible_characters(2)
+		repel_hud.set_align(Label.VALIGN_CENTER)
+		get_node("Centered").add_child(repel_hud)
 
 func _process(delta):
 	if is_network_master():
 		if Input.is_action_just_pressed("hero_2_switch_gravity"):
-			# Press button twice to cancel
-			var area = get_node("Area")
-			area.set_gravity(-area.get_gravity())
+			rpc("switch_gravity")
 			is_repelling = !is_repelling
 			if is_repelling:
-				get_node("RepellingHUD").set_text("/\\")
+				repel_hud.set_text("/\\")
 			else:
-				get_node("RepellingHUD").set_text("\\/")
+				repel_hud.set_text("\\/")
+
+sync func switch_gravity():
+	var area = get_node("Area")
+	area.set_gravity(-area.get_gravity())
+
