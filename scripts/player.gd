89947b4340437a2d84b24aff4e51259a3a6a008f
Luna <lucidlyluna@gmail.com>
2018-06-16 14:46:26 -0400

    [Hero 7] Fix glitchy camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -54,6 +54,7 @@ func _ready():
 	if is_network_master():
 		get_node("TPCamera/Camera/Ray").add_exception(self)
 		tp_camera.set_enabled(true)
+		tp_camera.add_collision_exception(self)
 		tp_camera.cam_view_sensitivity = 0.05
 		if "is_ai" in player_info and player_info.is_ai and not ai_instanced:
 			add_child(preload("res://scenes/ai.tscn").instance())
