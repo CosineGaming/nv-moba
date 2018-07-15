65d6044b995d6e35bb5828a03a7e96a1dd6c0a33
Luna <lucidlyluna@gmail.com>
2018-07-15 18:23:33 -0400

    Add working Voice Chat!!!
    NOTE: The changes are not all in this repository yet, cause I need to
    figure out submodules

--- py/VoiceChat.py
+++ py/VoiceChat.py
@@ -3,7 +3,7 @@
 from threading import Thread
 
 from godot import exposed
-from godot.bindings import Node2D, Vector2
+from godot.bindings import Control, Vector2, Input
 
 import pyaudio
 import pymumble_py3 as pymumble
@@ -13,47 +13,61 @@ RATE = 48000
 CHUNK = 1024
 
 @exposed
-class VoiceChat(Node2D):
+class VoiceChat(Control):
 
 	# Audio setup
 	p = pyaudio.PyAudio()
-	stream = p.open(
+	record = p.open(
 			format=pyaudio.paInt16,
 			channels=1,
 			rate=RATE,
 			input=True,
 			frames_per_buffer=CHUNK)
+	play = p.open(
+			format=pyaudio.paInt16,
+			channels=1,
+			rate=RATE,
+			output=True,
+			frames_per_buffer=CHUNK)
+	# Mumble setup
 	mumble = Mumble(
 			"192.168.1.13",
-			"luna-nv-moba",
-			debug=True)
+			"luna-nv-moba")
+
+	_speaking = []
 
 	def _ready(self):
 		# TODO: Get name in parameter and use it
 		# Mumble setup
-		self.set_position(Vector2(100, 15))
 		self.mumble.set_application_string("Godot voice-chat")
+		# You have to enable receiving sound fsr
+		self.mumble.set_receive_sound(True)
 		self.mumble.callbacks.set_callback(
-				pymumble.constants.PYMUMBLE_CLBK_CONNECTED,
-				self._success)
+				pymumble.constants.PYMUMBLE_CLBK_SOUNDRECEIVED,
+				self._sound_received)
 		self._loop()
-		self.mumble.set_bandwidth(200000)
-		# run_mumble = Thread(target=self._loop)
-		# run_mumble.start()
-		# self.mumble.is_ready()
+		# Actually waits until ready, despite name
+		self.mumble.is_ready()
 
 	def _loop(self):
 		self.mumble.start()
-	
-	def _success(self):
-		print("succeeedededd")
-	
+
 	def _process(self, delta):
-		data = self.stream.read(CHUNK)
-		# self.mumble.sound_output.add_sound(data)
-	
+		# We have to get the data out of the buffer either way
+		data = self.record.read(CHUNK)
+		# But only add it to mumble if we're pushing
+		if Input.is_action_pressed("push_to_talk"):
+			self.mumble.sound_output.add_sound(data)
+
+	def _sound_received(self, user, sound_chunk):
+		print(user.get_property("name"))
+		self.play.write(sound_chunk.pcm)
+
 	def _exit_tree(self):
-		self.stream.stop_stream()
-		self.stream.close()
+		# Close down all the PyAudio stuff
+		self.record.stop_stream()
+		self.record.close()
+		self.play.stop_stream()
+		self.play.close()
 		self.p.terminate()
 
