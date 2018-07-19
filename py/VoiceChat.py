9ee20cf3b65e611031522d59806eea6c6b657028
Luna <lucidlyluna@gmail.com>
2018-07-18 22:00:51 -0400

    Add text chat!

--- py/VoiceChat.py
+++ py/VoiceChat.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+# Bridge between Godot and pymumble
+# Must be called VoiceChat.py or godot-python acts weird
+
 from threading import Thread
 
 from godot import exposed
-from godot.bindings import Node, Vector2, Input, Array
+from godot.bindings import Node, Vector2, Input, Array, Dictionary
 
 import pyaudio
 import pymumble_py3 as pymumble
@@ -18,12 +21,14 @@ class VoiceChat(Node):
 
 	# To be initialized on connect
 	mumble = None
+	channel = None
 	record = None
 	play = None
 
 	_speaking = {}
+	_text_messages = []
 
-	def connect(self, channel, name):
+	def connect_to_server(self, channel, name):
 		# Mumble setup
 		self.mumble = Mumble(
 				"192.168.1.13",
@@ -34,17 +39,28 @@ class VoiceChat(Node):
 		self.mumble.callbacks.set_callback(
 				pymumble.constants.PYMUMBLE_CLBK_SOUNDRECEIVED,
 				self._sound_received)
+		self.mumble.callbacks.set_callback(
+				pymumble.constants.PYMUMBLE_CLBK_TEXTMESSAGERECEIVED,
+				self._text_received)
 		self.mumble.start()
 		# Actually waits until ready, despite name
 		self.mumble.is_ready()
 		# Move into the channel for the server we're on
 		# These are created prior because pymumble doesn't support dynamically creating them
-		self.mumble.channels.find_by_name(channel).move_in()
+		self.channel = self.mumble.channels.find_by_name(channel)
+		self.channel.move_in()
 
 		self._init_audio()
 
 	def get_speaking(self):
 		return Array(self._speaking.keys())
+
+	def send_message(self, message):
+		self._text_messages.append(message)
+		self.channel.send_text_message(message)
+
+	def get_messages(self):
+		return Array(self._text_messages)
 
 	def _init_audio(self):
 		# Audio setup
@@ -82,6 +98,11 @@ class VoiceChat(Node):
 			self.play.write(sound_chunk.pcm)
 			SPEAK_TIMEOUT = 0.1 # In seconds
 			self._speaking[name] = SPEAK_TIMEOUT
+	
+	def _text_received(self, message):
+		# Convert to Godot friendly format
+		text = message.message
+		self._text_messages.append(text)
 
 	def _exit_tree(self):
 		# Close down all the PyAudio stuff
--- scripts/mumble_ui.gd
+++ scripts/mumble_ui.gd
@@ -3,11 +3,14 @@ extends Control
 # The interface to the actual mumble client written in Python
 onready var mumble = get_node("Mumble")
 onready var speakers_node = get_node("Speakers")
+onready var textbox = get_node("Layer/Solid/Enter")
+onready var chatlog = get_node("Layer/Chat")
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	mumble.connect(str(networking.port), _get_mumble_id())
+	mumble.connect_to_server(str(networking.port), _get_mumble_id())
+	textbox.connect("text_entered", self, "_send_message")
 
 func _get_mumble_id():
 	return str(get_tree().get_network_unique_id())
@@ -15,6 +18,27 @@ func _get_mumble_id():
 func _get_nickname(id):
 	var data = networking.players[int(id)]
 	return data.username
+
+func _send_message(text):
+	# Unfortunately this callback is called WHENEVER we press enter, even if
+	# not focused. Checking the focus prevents us from sending empty messages
+	# when trying to focus, and releasing focus immediately after gaining
+	if textbox.has_focus():
+		# Pymumble doesn't support text messages "from" fields, so we have to
+		# do it manually :(
+		var our_nick = _get_nickname(get_tree().get_network_unique_id())
+		var fulltext = "<%s> %s" % [our_nick, text]
+		mumble.send_message(fulltext)
+		textbox.release_focus()
+	textbox.text = ""
+
+func _input(input):
+	if input.is_action_pressed("focus_chat"):
+		# We need to check if we have focus cause we don't wanna refocus when we're trying to DEfocus
+		if not textbox.has_focus():
+			# We'll release immediately unless we call deferred and wait for the
+			# _send_message has_focus check
+			textbox.call_deferred("grab_focus")
 
 func _process(delta):
 	var speaking = mumble.get_speaking()
@@ -26,4 +50,10 @@ func _process(delta):
 		speaker.get_node("Speaker").text = _get_nickname(id)
 		speaker.rect_position = Vector2(0, 60 * i)
 		speakers_node.add_child(speaker)
+
+	var text = "MESSAGES:"
+	for m in mumble.get_messages():
+		text += m + "\n"
+	# chatlog.text = text
+	chatlog.set_text(text)
 
